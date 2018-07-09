692df2022d184aacca032f9feb65f51fb10edf0c
Marcellino Ornelas <marcellino.ornelas.com>
2018-07-09 15:33:03 -0700

    challenge_2: server up and running

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,18 @@
+const express = require('express');
+const path = require('path')
 
+const app = express();
 
+app.get('/', function(req, res) {
+  
+  res.sendFile( path.join(__dirname,'client/index.html') )
+
+});
+
+app.get('/csv', function(req, res) {
+
+});
+
+app.listen(3000,function() {
+  console.log('SERVER IS NOW RUNNING ON PORT 3000');
+});
