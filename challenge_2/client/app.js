f252262d10722b17b2f38816ef989731e6c4b2ca
Marcellino Ornelas <marcellino.ornelas.com>
2018-07-09 15:45:21 -0700

    ajax request is reaching server

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,27 @@
+$(function(){
+
+  var $csvConverter = $('form');
+  var $results = $('#results');
+  var $json = $('#json');
+  
 
 
+  /*
+   * Events 
+  */
+
+  $csvConverter.on('submit', function(event) {
+    
+    event.preventDefault();
+
+
+
+    $.post('/csv', $json.val(), function(data) {
+      
+      console.log('we hit home base');
+
+    });
+
+  });
+
+});
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,14 +3,16 @@ const path = require('path')
 
 const app = express();
 
+app.use( express.static(__dirname + '/client') )
+
 app.get('/', function(req, res) {
   
   res.sendFile( path.join(__dirname,'client/index.html') )
 
 });
 
-app.get('/csv', function(req, res) {
-
+app.post('/csv', function(req, res) {
+  res.send('you hit me')
 });
 
 app.listen(3000,function() {
